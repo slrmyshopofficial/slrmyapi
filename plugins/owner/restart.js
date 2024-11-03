b330f10ed8c9cc564f7b848ee1222f1c8d7c8ad7
slrmyshopofficial <slrmybot@gmail.com>
2024-11-03 09:29:09 +0800

    Update restart.js

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -4,7 +4,7 @@ exports.run = {
    async: async (m, {
       clips
    }) => {
-      await clips.reply(m.chat, Func.texted('bold', '{ SlrmyApi } 🔐 restart is proccessing /\-/\'), m).then(async () => {
+      await clips.reply(m.chat, Func.texted('bold', '{ SlrmyApi } 🔐 restart is proccessing.........'), m).then(async () => {
          await props.save()
          process.send('reset')
       })
